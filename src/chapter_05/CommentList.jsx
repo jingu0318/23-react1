3de2217541f695e9fa5711fee5d71fe38e52daa9
jingu0318 <18dlwlsrn@gmail.com>
2023-04-06 12:13:06 +0900

    5장 CommentList 수정

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,26 +1,31 @@
-import React from "react"; // 컴포넌트 만들때는 리액트 임포트가 기본이다
-import Comment from "./Comment"
+import React from "react"; //컴포넌트 만들때는 리액트 임포트가 기본이다.
+import Comment from "./Comment";
 
-function CommentList(props){
-  return(
-    <div>
-      <Comment name={"2진구"} comment={"안녕하세요. 2진구입니다"}/>
-      <Comment name={"2진9"} comment={"안녕하세요. 2진9입니다"}/>
-      <Comment name={"279"} comment={"안녕하세요. 279입니다"}/>
-    </div>
-  )
-}
+const comments = [
+    {
+        name: "2진구",
+        comment: "안녕하세요, 2진구입니다.",
+    },
+    {
+        name: "2진9",
+        comment: "리액트 2진9!",
+    },
+    {
+        name: "279",
+        comment: "저도 279 배워보고 싶어요!!",
+    },
+];
 
-function CommentList(props){
-  return(
-    <div>
-      {CommentList.map((foo)=> {
-        return(
-          <Comment name={foo.name} comment={foo.comment}/>
-        )
-      })}
-    </div>
-  )
+function CommentList(props) {
+    return (
+        <div>
+            {comments.map((comment) => {
+                return (
+                    <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
+        </div>
+    );
 }
 
 export default CommentList;
