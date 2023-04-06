5e57e1520a55213e27e950c45493c74c8c487d8c
jingu0318 <18dlwlsrn@gmail.com>
2023-04-06 11:11:54 +0900

    코멘트리스트 사용할 수 있게 작성

--- src/chapter_05/Comment.jsx
+++ src/chapter_05/Comment.jsx
@@ -34,8 +34,18 @@ const styles = {
 
 function Comment(props){
   return(
-    <div>
-      <h1>내가 만든 첫 컴포넌트.</h1>
+    <div style={styles.wrapper}>
+      <div style={styles.imageContainer}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+          alt="프로필 이미지"
+          style={styles.image}
+       />
+      </div>
+      <div style={styles.contentContainer}>
+        <span style={styles.nameText}>{props.name}</span>
+        <span style={styles.commentText}>{props.comment}</span>
+      </div>
     </div>
   )
 }
