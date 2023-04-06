8ac3df461b455d74ec81ab5689f3ac3356fa1a0d
jingu0318 <18dlwlsrn@gmail.com>
2023-04-06 11:12:17 +0900

    코멘트 리스트들 내용 삽입

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -4,7 +4,9 @@ import Comment from "./Comment"
 function CommentList(props){
   return(
     <div>
-      <comment />
+      <Comment name={"2진구"} comment={"안녕하세요. 2진구입니다"}/>
+      <Comment name={"2진9"} comment={"안녕하세요. 2진9입니다"}/>
+      <Comment name={"279"} comment={"안녕하세요. 279입니다"}/>
     </div>
   )
 }
