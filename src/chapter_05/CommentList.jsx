ad952b96f68fd1cafd2d2d01e210ffda8e120314
jingu0318 <18dlwlsrn@gmail.com>
2023-04-06 10:39:51 +0900

    commentList 와 index 내용 삽입

---
+++ src/chapter_05/CommentList.jsx
@@ -0,0 +1,12 @@
+import React from "react"; // 컴포넌트 만들때는 리액트 임포트가 기본이다
+import Comment from "./Comment"
+
+function CommentList(props){
+  return(
+    <div>
+      <comment />
+    </div>
+  )
+}
+
+export default CommentList;
