906e6f4df478d332dc93ccea3e5b1d5895aee2b9
jingu0318 <18dlwlsrn@gmail.com>
2023-05-04 11:50:10 +0900

    11장 index.js 수정

--- src/index.js
+++ src/index.js
@@ -13,12 +13,13 @@ import Apple from './pt_1/Apple';
 import ConfirmButton from './chapter_08/ConfirmButton';
 import LandingPage from './chapter_09/LandingPage'
 import AttendanceBook from './chapter_10/AttendanceBook'
+import SignUp from './chapter_11/SignUp';
 
 
   const root = ReactDOM.createRoot(document.getElementById('root'));
   root.render(
     <React.StrictMode>
-      <AttendanceBook />  
+      <SignUp />  
     </React.StrictMode>
   );
 
