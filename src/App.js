a8d6fcd4ff9c6f5c67b3ce44557b044830a2b8a9
jingu0318 <18dlwlsrn@gmail.com>
2023-05-11 11:47:14 +0900

    중간 app.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import { Component,useState } from 'react';
 
 
-class Subject extends Component {
-  render() {
+const App = () => {
     const [fruit, setfruit] = useState();
 
   const onApple = () => {
@@ -19,22 +18,11 @@ class Subject extends Component {
     return (
       <div>
       <h1>어떤 과일을 좋아하나요? {fruit}.</h1>
-      <button onClick={onApple}>사과</button> 
-      <button onClick={onOrange}>오렌지</button>
+      <button onClick={onApple}>사과</button>  &nbsp;&nbsp;&nbsp;
+      <button onClick={onOrange}>오렌지</button> &nbsp;&nbsp;&nbsp;
       <button onClick={onBanana}>바나나</button>
     </div>
     );
-  }
-}
-
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Subject></Subject>
-      </div>
-    );
-  }
-}
+  };
 
 export default App;
